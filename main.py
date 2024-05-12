a5c2267a4c9accf2ece2b2b7aa56a0d2d6437262
Ghady Abboud <abboudghady@gmail.com>
2024-05-11 23:46:41 -0400

    Created the table for the data and styled it. Shortened the name for the nutrients

--- main.py
+++ main.py
@@ -10,27 +10,37 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 import requests
 
-#response = requests.get('https://api.nal.usda.gov/fdc/v1/foods/list?api_key=n0REEKBm9KoyfBmL0UneSt5WSej8lzTmziYBpncl') 
-#data = response.json() # Parse the content of of an HTTP reponse as JSON format 
-
 
 app = Flask(__name__)
 
-#@app.route('/foods/search') 
-def search(query): # Food Search function 
+def search(query): 
 
-    #query = request.args.get('query') # This gets the query from the user then appends it to the api url
 
     API_URL = f"https://api.nal.usda.gov/fdc/v1/foods/search?api_key=n0REEKBm9KoyfBmL0UneSt5WSej8lzTmziYBpncl&query={query}"
-    response = requests.get(API_URL) # This retrieves the HTTP response of the query 
+    response = requests.get(API_URL) 
 
-
-    # If the query from the user was successful, then filter out the data by the relevant nutrients 
     try:
         if response.status_code == 200:
             search_results = response.json()
+            
+            shorter_nutrients = {
+                "Protein" : "Protein",
+                "Total lipid (fat)" : "Fat",
+                "Energy": "Energy",
+                "Total Sugars":"Total Sugar",
+                "Fiber, total dietary" : "Fiber",
+                "Calcium, Ca": "Calcium",
+                "Iron, Fe": "Iron",
+                "Sodium, Na":"Sodium",
+                "Vitamin A, IU":"Vitamin A",
+                "Vitamin C, total ascorbic acid" : "Vitamin C",
+                "Cholesterol": "Cholesterol",
+                "Fatty acids, total trans": "Trans Fat",
+                "Fatty acids, total saturated":"Saturated Fat",
+                "Carbohydrate, by difference" :"Carbohydrates"
+            }
 
-            relevant_nutrients = ['Protein',
+            relevant_nutrients = ['Protein',    
                                 "Total lipid (fat)",
                                 "Energy",
                                 "Total Sugars",
@@ -50,11 +60,17 @@ def search(query): # Food Search function
             nutrients = [] 
             for nutrient in search_results['foods'][0]['foodNutrients']:
                 if nutrient['nutrientName'] in relevant_nutrients:
+                    
+                    if nutrient['nutrientName'] in shorter_nutrients:
+                        # If it is, use the shorter version
+                        nutrient_name = shorter_nutrients[nutrient['nutrientName']]
+                        
                     nutrient_info = {
-                        'name': nutrient['nutrientName'],
+                        'name': nutrient_name,
                         'value': nutrient['value'],
                         'unit': nutrient['unitName']
                     }
+                    
                     nutrients.append(nutrient_info)
             
             filtered_data = {
@@ -62,7 +78,7 @@ def search(query): # Food Search function
                 'Nutrients' : nutrients
             }
             
-            return filtered_data # Return the retrieved data as a dict
+            return filtered_data 
         
     except:
         return "Error: Unable to retrieve search results"
@@ -70,7 +86,7 @@ def search(query): # Food Search function
 
 @app.route('/', methods = ['GET','POST'])
 def home():
-    if request.method == 'POST' : # Generated when a user submits a form by clicking the submit button 
+    if request.method == 'POST' :
         new_query = request.form['query']
         results = search(query = new_query)
         return render_template('index.html',results=results)
